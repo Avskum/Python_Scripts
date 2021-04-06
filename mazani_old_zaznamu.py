6ae4b3760390aa3bddd0875b0deb6b069bd79690
Avskum <masirrobert@gmail.com>
2021-04-06 17:42:37 +0200

    mazani_old_zaznamu.py

---
+++ mazani_old_zaznamu.py
@@ -0,0 +1,53 @@
+import pymysql.cursors
+import shutil
+import os
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='pwd',
+                             db='system_mail',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+cursor = connection.cursor()
+sql="SELECT id, name, state FROM domain WHERE state='2'"
+cursor.execute(sql)
+records = cursor.fetchall()
+print("Total number of records for deleting is: ", cursor.rowcount)
+
+cursor.execute("select distinct name from domain where state = 2")
+deleted = set(row['name'] for row in cursor.fetchall())
+
+cursor.execute("select distinct name from domain where state != 2")
+active = set(row['name'] for row in cursor.fetchall())
+
+to_delete = deleted - active
+
+print('Printing each domain record', "\n")
+for row in records:
+
+    print("id = ", row["id"], )
+    print("name = ", row["name"])
+    print("state  = ", row["state"], "\n")
+
+    id = row["id"]
+    name = row["name"]
+    state = row["state"]
+
+    if to_delete:
+        try:
+            if os.path.exists('/data/sa/' + name):
+                print('found records for deleting: ' + name, "\n")
+                print("Total number of records for deleting is: ", cursor.rowcount)
+                input("Press Enter to continue...")
+                shutil.rmtree('/data/sa/' + name)
+                print('records deleted')            
+            else:
+                print('no Directory found')
+        except Exception as error:
+            print("Directory already deleted or never existed")
+    else:
+        print('no records for deleting found')
+        print('domain', name)
+        print('hast state', state, "\n")
+
+quit()
+connection.close()
